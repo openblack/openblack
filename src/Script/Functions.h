d25b8d8bc22f3b53bf5bedca0bbcc6cedca6fbfd
Matt Stevens <matt@handsomematt.co.uk>
2018-11-15 01:00:39 +0000

    Make LHScriptX callable functions static instead of instanced.

--- src/Script/Functions.h
+++ src/Script/Functions.h
@@ -20,10 +20,6 @@ namespace OpenBlack
 
 		class FunctionContext;
 
-
-
-
-
 		class Variable {
 		private:
 			Type _type;
@@ -38,7 +34,7 @@ namespace OpenBlack
 
 		class Functions {
 		private:
-			typedef void (Functions::*funcPtr)(FunctionContext &ctx);
+			typedef void (*funcPtr)(FunctionContext &ctx);
 
 			struct FunctionPointer {
 				uint32_t id;
@@ -46,163 +42,139 @@ namespace OpenBlack
 				Type parameters[12];
 				funcPtr func;
 			};
+			
+			static void createMist(FunctionContext &ctx);
+			static void createPath(FunctionContext &ctx);
+			static void createTown(FunctionContext &ctx);
+			static void setTownBelief(FunctionContext &ctx);
+			static void setTownBeliefCap(FunctionContext &ctx);
+			static void setTownUninhabitable(FunctionContext &ctx);
+			static void setTownCongregationPos(FunctionContext &ctx);
+			static void createAbode(FunctionContext &ctx);
+			static void createPlannedAbode(FunctionContext &ctx);
+			static void createTownCentre(FunctionContext &ctx);
+			static void createTownSpell(FunctionContext &ctx);
+			static void createNewTownSpell(FunctionContext &ctx);
+			static void createTownCentreSpellIcon(FunctionContext &ctx);
+			static void createSpellIcon(FunctionContext &ctx);
+			static void createPlannedSpellIcon(FunctionContext &ctx);
+			static void createVillager(FunctionContext &ctx);
+			static void createTownVillager(FunctionContext &ctx);
+			static void createSpecialTownVillager(FunctionContext &ctx);
+			static void createVillagerPos(FunctionContext &ctx);
+			static void createCitadel(FunctionContext &ctx);
+			static void createPlannedCitadel(FunctionContext &ctx);
+			static void createCreaturePen(FunctionContext &ctx);
+			static void createWorshipSite(FunctionContext &ctx);
+			static void createPlannedWorshipSite(FunctionContext &ctx);
+			static void createAnimal(FunctionContext &ctx);
+			static void createNewAnimal(FunctionContext &ctx);
+			static void createForest(FunctionContext &ctx);
+			static void createTree(FunctionContext &ctx);
+			static void createNewTree(FunctionContext &ctx);
+			static void createField(FunctionContext &ctx);
+			static void createTownField(FunctionContext &ctx);
+			static void createFishFarm(FunctionContext &ctx);
+			static void createTownFishFarm(FunctionContext &ctx);
+			static void createFeature(FunctionContext &ctx);
+			static void createFlowers(FunctionContext &ctx);
+			static void createWallSection(FunctionContext &ctx);
+			static void createPlannedWallSection(FunctionContext &ctx);
+			static void createPitch(FunctionContext &ctx);
+			static void createPot(FunctionContext &ctx);
+			static void createTownTemporaryPots(FunctionContext &ctx);
+			static void createMobileobject(FunctionContext &ctx);
+			static void createMobilestatic(FunctionContext &ctx);
+			static void createMobileStatic(FunctionContext &ctx);
+			static void createDeadTree(FunctionContext &ctx);
+			static void createScaffold(FunctionContext &ctx);
+			static void countryChange(FunctionContext &ctx);
+			static void heightChange(FunctionContext &ctx);
+			static void createCreature(FunctionContext &ctx);
+			static void createCreatureFromFile(FunctionContext &ctx);
+			static void createFlock(FunctionContext &ctx);
+			static void loadLandscape(FunctionContext &ctx);
+			static void version(FunctionContext &ctx);
+			static void createArea(FunctionContext &ctx);
+			static void startCameraPos(FunctionContext &ctx);
+			static void flyByFile(FunctionContext &ctx);
+			static void townNeedsPos(FunctionContext &ctx);
+			static void createFurniture(FunctionContext &ctx);
+			static void createBigForest(FunctionContext &ctx);
+			static void createNewBigForest(FunctionContext &ctx);
+			static void createInfluenceRing(FunctionContext &ctx);
+			static void createWeatherClimate(FunctionContext &ctx);
+			static void createWeatherClimateRain(FunctionContext &ctx);
+			static void createWeatherClimateTemp(FunctionContext &ctx);
+			static void createWeatherClimateWind(FunctionContext &ctx);
+			static void createWeatherStorm(FunctionContext &ctx);
+			static void brushSize(FunctionContext &ctx);
+			static void createStream(FunctionContext &ctx);
+			static void createStreamPoint(FunctionContext &ctx);
+			static void createWaterfall(FunctionContext &ctx);
+			static void createArena(FunctionContext &ctx);
+			static void createFootpath(FunctionContext &ctx);
+			static void createFootpathNode(FunctionContext &ctx);
+			static void linkFootpath(FunctionContext &ctx);
+			static void createBonfire(FunctionContext &ctx);
+			static void createBase(FunctionContext &ctx);
+			static void createNewFeature(FunctionContext &ctx);
+			static void setInteractDesire(FunctionContext &ctx);
+			static void toggleComputerPlayer(FunctionContext &ctx);
+			static void setComputerPlayerCreatureLike(FunctionContext &ctx);
+			static void multiplayerDebug(FunctionContext &ctx);
+			static void createStreetLantern(FunctionContext &ctx);
+			static void createStreetLight(FunctionContext &ctx);
+			static void setLandNumber(FunctionContext &ctx);
+			static void createOneShotSpell(FunctionContext &ctx);
+			static void createOneShotSpellPu(FunctionContext &ctx);
+			static void createFireFly(FunctionContext &ctx);
+			static void townDesireBoost(FunctionContext &ctx);
+			static void createAnimatedStatic(FunctionContext &ctx);
+			static void fireFlySpellRewardProb(FunctionContext &ctx);
+			static void createNewTownField(FunctionContext &ctx);
+			static void createSpellDispenser(FunctionContext &ctx);
+			static void loadComputerPlayerPersonallty(FunctionContext &ctx);
+			static void setComputerPlayerPersonallty(FunctionContext &ctx);
+			static void setGlobalLandBalance(FunctionContext &ctx);
+			static void setLandBalance(FunctionContext &ctx);
+			static void createDrinkWaypoint(FunctionContext &ctx);
+			static void setTownInfluenceMultiplier(FunctionContext &ctx);
+			static void setPlayerInfluenceMultiplier(FunctionContext &ctx);
+			static void setTownBalanceBeliefScale(FunctionContext &ctx);
+			static void startGameMessage(FunctionContext &ctx);
+			static void addGameMessageLine(FunctionContext &ctx);
+			static void editLevel(FunctionContext &ctx);
+			static void setNighttime(FunctionContext &ctx);
+			static void makeLastObjectArtifact(FunctionContext &ctx);
+			static void setLostTownScale(FunctionContext &ctx);
 
-			//static const FunctionPointer kFunctionPointers[];
+			/* Map functions */
+			static void setNoPlayers(FunctionContext &ctx);
+			static void loadTribeDance(FunctionContext &ctx);
+			static void setDate(FunctionContext &ctx);
+			static void setTime(FunctionContext &ctx);
+			static void setTurnsPerYear(FunctionContext &ctx);
+			static void setGameTickTime(FunctionContext &ctx);
+			static void loadFeatureScript(FunctionContext &ctx);
+			static void pauseGame(FunctionContext &ctx);
+			//static void createCreature(FunctionContext &ctx);
+			static void outputVillagers(FunctionContext &ctx);
+			static void outputTown(FunctionContext &ctx);
+			static void outputCreatures(FunctionContext &ctx);
+			static void outputCollide(FunctionContext &ctx);
+			static void outputAlloc(FunctionContext &ctx);
+			static void saveForNetDebug(FunctionContext &ctx);
+			static void loadForNetDebug(FunctionContext &ctx);
+			//static void loadLandscape(FunctionContext &ctx);
+			static void loadGameScript(FunctionContext &ctx);
+			static void loadRawGameScript(FunctionContext &ctx);
+			static void loadLanguage(FunctionContext &ctx);
 
-			void createMist(FunctionContext &ctx);
-			void createPath(FunctionContext &ctx);
-			void createTown(FunctionContext &ctx);
-			void setTownBelief(FunctionContext &ctx);
-			void setTownBeliefCap(FunctionContext &ctx);
-			void setTownUninhabitable(FunctionContext &ctx);
-			void setTownCongregationPos(FunctionContext &ctx);
-			void createAbode(FunctionContext &ctx);
-			void createPlannedAbode(FunctionContext &ctx);
-			void createTownCentre(FunctionContext &ctx);
-			void createTownSpell(FunctionContext &ctx);
-			void createNewTownSpell(FunctionContext &ctx);
-			void createTownCentreSpellIcon(FunctionContext &ctx);
-			void createSpellIcon(FunctionContext &ctx);
-			void createPlannedSpellIcon(FunctionContext &ctx);
-			void createVillager(FunctionContext &ctx);
-			void createTownVillager(FunctionContext &ctx);
-			void createSpecialTownVillager(FunctionContext &ctx);
-			void createVillagerPos(FunctionContext &ctx);
-			void createCitadel(FunctionContext &ctx);
-			void createPlannedCitadel(FunctionContext &ctx);
-			void createCreaturePen(FunctionContext &ctx);
-			void createWorshipSite(FunctionContext &ctx);
-			void createPlannedWorshipSite(FunctionContext &ctx);
-			void createAnimal(FunctionContext &ctx);
-			void createNewAnimal(FunctionContext &ctx);
-			void createForest(FunctionContext &ctx);
-			void createTree(FunctionContext &ctx);
-			void createNewTree(FunctionContext &ctx);
-			void createField(FunctionContext &ctx);
-			void createTownField(FunctionContext &ctx);
-			void createFishFarm(FunctionContext &ctx);
-			void createTownFishFarm(FunctionContext &ctx);
-			void createFeature(FunctionContext &ctx);
-			void createFlowers(FunctionContext &ctx);
-			void createWallSection(FunctionContext &ctx);
-			void createPlannedWallSection(FunctionContext &ctx);
-			void createPitch(FunctionContext &ctx);
-			void createPot(FunctionContext &ctx);
-			void createTownTemporaryPots(FunctionContext &ctx);
-			void createMobileobject(FunctionContext &ctx);
-			void createMobilestatic(FunctionContext &ctx);
-			void createMobileStatic(FunctionContext &ctx);
-			void createDeadTree(FunctionContext &ctx);
-			void createScaffold(FunctionContext &ctx);
-			void countryChange(FunctionContext &ctx);
-			void heightChange(FunctionContext &ctx);
-			void createCreature(FunctionContext &ctx);
-			void createCreatureFromFile(FunctionContext &ctx);
-			void createFlock(FunctionContext &ctx);
-			void loadLandscape(FunctionContext &ctx);
-			void version(FunctionContext &ctx);
-			void createArea(FunctionContext &ctx);
-			void startCameraPos(FunctionContext &ctx);
-			void flyByFile(FunctionContext &ctx);
-			void townNeedsPos(FunctionContext &ctx);
-			void createFurniture(FunctionContext &ctx);
-			void createBigForest(FunctionContext &ctx);
-			void createNewBigForest(FunctionContext &ctx);
-			void createInfluenceRing(FunctionContext &ctx);
-			void createWeatherClimate(FunctionContext &ctx);
-			void createWeatherClimateRain(FunctionContext &ctx);
-			void createWeatherClimateTemp(FunctionContext &ctx);
-			void createWeatherClimateWind(FunctionContext &ctx);
-			void createWeatherStorm(FunctionContext &ctx);
-			void brushSize(FunctionContext &ctx);
-			void createStream(FunctionContext &ctx);
-			void createStreamPoint(FunctionContext &ctx);
-			void createWaterfall(FunctionContext &ctx);
-			void createArena(FunctionContext &ctx);
-			void createFootpath(FunctionContext &ctx);
-			void createFootpathNode(FunctionContext &ctx);
-			void linkFootpath(FunctionContext &ctx);
-			void createBonfire(FunctionContext &ctx);
-			void createBase(FunctionContext &ctx);
-			void createNewFeature(FunctionContext &ctx);
-			void setInteractDesire(FunctionContext &ctx);
-			void toggleComputerPlayer(FunctionContext &ctx);
-			void setComputerPlayerCreatureLike(FunctionContext &ctx);
-			void multiplayerDebug(FunctionContext &ctx);
-			void createStreetLantern(FunctionContext &ctx);
-			void createStreetLight(FunctionContext &ctx);
-			void setLandNumber(FunctionContext &ctx);
-			void createOneShotSpell(FunctionContext &ctx);
-			void createOneShotSpellPu(FunctionContext &ctx);
-			void createFireFly(FunctionContext &ctx);
-			void townDesireBoost(FunctionContext &ctx);
-			void createAnimatedStatic(FunctionContext &ctx);
-			void fireFlySpellRewardProb(FunctionContext &ctx);
-			void createNewTownField(FunctionContext &ctx);
-			void createSpellDispenser(FunctionContext &ctx);
-			void loadComputerPlayerPersonallty(FunctionContext &ctx);
-			void setComputerPlayerPersonallty(FunctionContext &ctx);
-			void setGlobalLandBalance(FunctionContext &ctx);
-			void setLandBalance(FunctionContext &ctx);
-			void createDrinkWaypoint(FunctionContext &ctx);
-			void setTownInfluenceMultiplier(FunctionContext &ctx);
-			void setPlayerInfluenceMultiplier(FunctionContext &ctx);
-			void setTownBalanceBeliefScale(FunctionContext &ctx);
-			void startGameMessage(FunctionContext &ctx);
-			void addGameMessageLine(FunctionContext &ctx);
-			void editLevel(FunctionContext &ctx);
-			void setNighttime(FunctionContext &ctx);
-			void makeLastObjectArtifact(FunctionContext &ctx);
-			void setLostTownScale(FunctionContext &ctx);
-
-			static const FunctionPointer kFunctionPointers[];
-
-
-			/*
-			void setNoPlayers(FunctionContext &ctx);
-			void loadTribeDance(FunctionContext &ctx);
-			void setDate(FunctionContext &ctx);
-			void setTime(FunctionContext &ctx);
-			void setTurnsPerYear(FunctionContext &ctx);
-			void setGameTickTime(FunctionContext &ctx);
-			void loadFeatureScript(FunctionContext &ctx);
-			void pauseGame(FunctionContext &ctx);
-			void createCreature(FunctionContext &ctx);
-			void outputVillagers(FunctionContext &ctx);
-			void outputTown(FunctionContext &ctx);
-			void outputCreatures(FunctionContext &ctx);
-			void outputCollide(FunctionContext &ctx);
-			void outputAlloc(FunctionContext &ctx);
-			void saveForNetDebug(FunctionContext &ctx);
-			void loadForNetDebug(FunctionContext &ctx);
-			void loadLandscape(FunctionContext &ctx);
-			void loadGameScript(FunctionContext &ctx);
-			void loadRawGameScript(FunctionContext &ctx);
-			void loadLanguage(FunctionContext &ctx);
-
-			const FunctionPointer kFunctionPointers[] = {
-				{ 0,   "SET_NO_PLAYERS",       { kTypeNumber },                                        &Functions::setNoPlayers },
-				{ 1,   "LOAD_TRIBE_DANCE",     { kTypeVector, kTypeNumber },                           &Functions::loadTribeDance },
-				{ 2,   "SET_DATE",             { kTypeNumber, kTypeNumber, kTypeNumber },              &Functions::setDate },
-				{ 3,   "SET_TIME",             { kTypeNumber, kTypeNumber, kTypeNumber },              &Functions::setTime },
-				{ 4,   "SET_TURNS_PER_YEAR",   { kTypeNumber },                                        &Functions::setTurnsPerYear },
-				{ 5,   "SET_GAME_TICK_TIME",   { kTypeNumber },                                        &Functions::setGameTickTime },
-				{ 6,   "LOAD_FEATURE_SCRIPT",  { kTypeVector },                                        &Functions::loadFeatureScript },
-				{ 7,   "PAUSE_GAME",           {  },                                                   &Functions::pauseGame },
-				{ 8,   "CREATE_CREATURE",      { kTypeNumber, kTypeNumber, kTypeNumber, kTypeNumber }, &Functions::createCreature },
-				{ 9,   "OUTPUT_VILLAGERS",     {  },                                                   &Functions::outputVillagers },
-				{ 10,  "OUTPUT_TOWN",          {  },                                                   &Functions::outputTown },
-				{ 11,  "OUTPUT_CREATURES",     {  },                                                   &Functions::outputCreatures },
-				{ 12,  "OUTPUT_COLLIDE",       {  },                                                   &Functions::outputCollide },
-				{ 13,  "OUTPUT_ALLOC",         {  },                                                   &Functions::outputAlloc },
-				{ 14,  "SAVE_FOR_NET_DEBUG",   { kTypeNumber, kTypeNumber },                           &Functions::saveForNetDebug },
-				{ 15,  "LOAD_FOR_NET_DEBUG",   { kTypeNumber },                                        &Functions::loadForNetDebug },
-				{ 16,  "LOAD_LANDSCAPE",       { kTypeVector },                                        &Functions::loadLandscape },
-				{ 17,  "LOAD_GAME_SCRIPT",     { kTypeVector },                                        &Functions::loadGameScript },
-				{ 18,  "LOAD_RAW_GAME_SCRIPT", { kTypeVector },                                        &Functions::loadRawGameScript },
-				{ 19,  "LOAD_LANGUAGE",        {  },                                                   &Functions::loadLanguage },
-			};
-			*/
+			static const FunctionPointer kLandFunctionPointers[];
+			static const FunctionPointer kMapFunctionPointers[];
+		public:
+			static FunctionPointer* GetFunctionFromName(std::string name);
 		};
 	}
 }
