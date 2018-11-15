726873e2c872c1556302639e554dc71482662a31
Matt Stevens <matt@handsomematt.co.uk>
2018-11-15 01:03:07 +0000

    After lexing, scan tokens for identifiers that match function names and match parameters given.

--- src/Script/LHScriptX.cpp
+++ src/Script/LHScriptX.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include <Script/Lexer.h>
+#include <Script/Functions.h>
 
 using namespace OpenBlack;
 using namespace OpenBlack::Script;
@@ -59,14 +60,35 @@ bool LHScriptX::LoadOneLine(OSFile* file)
 
 void LHScriptX::ScanLine(std::string line)
 {
-	printf("scanning: %s\n", line.c_str());
-
 	auto lexer = new Lexer(line);
-	auto tokens = lexer->GetTokens();
+	auto tokens = lexer->GetTokens();	
 
-	for (auto const& i : tokens) {
+	// todo: this should be turned into some bytecode rather then executing directly from tokens?
+	if (tokens.front().Type == TokenType::Identifier)
+	{
+		// assume it's a simple function call: Identifer, LeftPareth, Vars, RightPareth
+		auto identifier = tokens.front();
+		auto func = Functions::GetFunctionFromName(identifier.Value);
+
+		std::cout << identifier.Value << ": ID=" << func->id << "(";
+
+		for (int i = 0; i < 12; i++) {
+			Type param = func->parameters[i];
+			std::cout << param;
+			//std::cout << "\tP" << i << " Type = " << param << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		// next token should be: LeftParen
+	}
+	else {
+		throw std::logic_error(std::string{} +"Line " + line + " does not start with an identifier.");
+	}
+
+	/*for (auto const& i : tokens) {
 		std::cout << GetTokenTypeName(i.Type) << ": '" << i.Value << "'\n";
 	}
 
-	std::cout << std::endl;
+	std::cout << std::endl;*/
 }
