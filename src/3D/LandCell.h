e970d5e3faa9910d7273e7a4df06e0723a93fc23
Matt Stevens <matt@handsomematt.co.uk>
2018-11-18 00:18:50 +0000

    better describe LandCell properties

--- src/3D/LandCell.h
+++ src/3D/LandCell.h
@@ -9,21 +9,22 @@ namespace OpenBlack
 	class LandCell
 	{
 	public:
+		inline rgba_t Color()     { return color; }
 		inline uint8_t Altitude() { return altitude; }
-		inline uint8_t Country() { return flags1 & 0x0F; } // 0000 1111
-		inline bool Split() { return flags1 & 0x80; } // 1000 0000
-		inline bool HasWater() { return flags1 & 0x10; } // 0001 0000
-		inline bool Coastline() { return flags1 & 0x20; } // 0010 0000
-		inline bool FullWater() { return flags1 & 0x40; } // 0100 0000
+		inline bool Split()       { return properties & 0x80; }
+		inline bool HasWater()    { return properties & 0x10; }
+		inline bool Coastline()   { return properties & 0x20; }
+		inline bool FullWater()	  { return properties & 0x40; } 
+		inline uint8_t Country()  { return properties & 0x0F; }
 	private:
 		rgba_t color;
 
 		uint8_t altitude;
 		uint8_t savecolor;
 
+		uint8_t properties;
 		// Sound properties: coastal sound, land sound, sea sound, freshwater sound
-		uint8_t flags1; // 0x0F = country 0x80 = split // & 0x10 = water & 0x20 = coast
-		uint8_t flags2; // sound properties ..
+		uint8_t flags2;
 	};
 }
 
