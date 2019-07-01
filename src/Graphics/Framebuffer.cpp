42ee47aff56958bd39d618689e3d0fda7bd29846
Matt Stevens <matt@handsomematt.co.uk>
2019-07-01 01:30:07 +0100

    framebuffer object classes

---
+++ src/Graphics/Framebuffer.cpp
@@ -0,0 +1,55 @@
+/* OpenBlack - A reimplementation of Lionhead's Black & White.
+ *
+ * OpenBlack is the legal property of its developers, whose names
+ * can be found in the AUTHORS.md file distributed with this source
+ * distribution.
+ *
+ * OpenBlack is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenBlack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenBlack. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Graphics/FrameBuffer.h>
+#include <cassert>
+#include <stdexcept>
+
+namespace OpenBlack::Graphics
+{
+
+FrameBuffer::FrameBuffer(GLsizei width, GLsizei height, GLenum format) :
+    _handle(0), _width(width), _height(height), _format(format)
+{
+	glGenFramebuffers(1, &_handle);
+	assert(_handle);
+
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _handle);
+
+	_texture = new Texture2D(_width, _height, GL_RGBA, _format, GL_UNSIGNED_BYTE, NULL);
+	glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture->GetHandle(), 0);
+
+	if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		throw std::runtime_error("failed to create framebuffer");
+
+	// unbind it (restore state in future?)
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+}
+
+FrameBuffer::~FrameBuffer()
+{
+	if (_handle != 0)
+		glDeleteFramebuffers(1, &_handle);
+
+	if (_texture != nullptr)
+		delete _texture;
+}
+
+}
---
+++ src/Graphics/Framebuffer.h
@@ -0,0 +1,55 @@
+/* OpenBlack - A reimplementation of Lionhead's Black & White.
+ *
+ * OpenBlack is the legal property of its developers, whose names
+ * can be found in the AUTHORS.md file distributed with this source
+ * distribution.
+ *
+ * OpenBlack is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenBlack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenBlack. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <Graphics/OpenGL.h>
+#include <Graphics/Texture2D.h>
+
+namespace OpenBlack {
+namespace Graphics {
+
+class FrameBuffer {
+public:
+	FrameBuffer() = delete;
+	FrameBuffer(GLsizei width, GLsizei height, GLenum format);
+	~FrameBuffer();
+
+	void Bind() { glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _handle); }
+	void Unbind() { glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); }
+
+	Texture2D* GetTexture() { return _texture; }
+
+	//inline void Bind() { glBindTexture(GL_TEXTURE_RECTANGLE, _textureID); }
+
+	const GLsizei GetWidth() const { return _width; }
+	const GLsizei GetHeight() const { return _height; }
+private:
+	GLuint _handle;
+
+	GLsizei _width;
+	GLsizei _height;
+	GLenum _format;
+
+	Texture2D* _texture;
+};
+
+}
+}
