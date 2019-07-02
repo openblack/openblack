d902b48d6965954d1105c7dab61c10a1d5ce4e49
Matt Stevens <matt@handsomematt.co.uk>
2019-07-03 00:50:35 +0100

    mess around with face culling, need to wind up verts properly

--- src/Game.cpp
+++ src/Game.cpp
@@ -242,6 +242,10 @@ void Game::drawScene(const Camera& camera, bool drawWater)
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	//glFrontFace(GL_CCW);
+
 	_sky->Draw(camera);
 
 	if (drawWater)
