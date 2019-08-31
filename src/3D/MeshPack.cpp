56e89cc29641def9138d3c359b357e0c89ae7fc0
NeroBurner-tux <pyro4hell@gmail.com>
2019-08-31 21:08:09 +0200

    use snprintf because iota is not ansi C++

--- src/3D/MeshPack.cpp
+++ src/3D/MeshPack.cpp
@@ -25,6 +25,7 @@
 #include <algorithm>
 #include <stdexcept>
 #include <stdint.h>
+#include <stdlib.h> // snprintf
 
 namespace OpenBlack
 {
@@ -158,7 +159,7 @@ void MeshPack::loadTextures(File& file)
 	char sBlockID[4];
 	for (auto const& tex : textureTypeMap)
 	{
-		itoa(tex.first, sBlockID, 16);
+		snprintf(sBlockID, sizeof(sBlockID), "%x", tex.first);
 
 		auto const& textureBlock = _blocks.find(sBlockID);
 		if (textureBlock == _blocks.end())
