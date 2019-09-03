da703e38254d424122d13d1a919bca6b7aaf6791
NeroBurner-tux <pyro4hell@gmail.com>
2019-09-03 18:13:22 +0200

    hunter: manually provide EnTT and use it

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,3 +1,8 @@
 hunter_config(glm
   VERSION ${HUNTER_glm_VERSION}
   CMAKE_ARGS GLM_TEST_ENABLE=OFF)
+
+hunter_config(EnTT
+	VERSION 3.1.0
+	URL "https://github.com/skypjack/entt/archive/v3.1.0.tar.gz"
+	SHA1 "eb59eef176d934a5ea35c4160f5f87b573d850aa")
