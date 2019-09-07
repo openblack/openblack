90d011b5e98d6e38fad895450f3bbee7491ad1f5
Sandy Carter <bwrsandman@gmail.com>
2019-09-07 12:09:18 +0200

    Gui: Move imgui to static GUI functions

---
+++ src/Gui.h
@@ -0,0 +1,40 @@
+/* OpenBlack - A reimplementation of Lionhead's Black & White.
+ *
+ * OpenBlack is the legal property of its developers, whose names
+ * can be found in the AUTHORS.md file distributed with this source
+ * distribution.
+ *
+ * OpenBlack is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenBlack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenBlack. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+typedef void *SDL_GLContext;
+union SDL_Event;
+
+namespace OpenBlack
+{
+class Game;
+class GameWindow;
+class MeshViewer;
+
+class Gui
+{
+  public:
+	static void Init(const GameWindow &window, const SDL_GLContext &context, float scale);
+	static void ProcessSDLEvent(const SDL_Event& event);
+	static void Loop(Game& game, MeshViewer& meshViewer);
+	static void Draw();
+};
+} // namespace OpenBlack
