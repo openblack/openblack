26ca5ae981f5abdb562e5433c4b9117eecb30978
Sandy Carter <bwrsandman@gmail.com>
2019-09-07 16:22:03 +0200

    Put all mentions to OpenBlack in comments lower-case

--- src/Common/FileStream.h
+++ src/Common/FileStream.h
@@ -1,21 +1,21 @@
-/* OpenBlack - A reimplementation of Lionhead's Black & White.
+/* openblack - A reimplementation of Lionhead's Black & White.
  *
- * OpenBlack is the legal property of its developers, whose names
+ * openblack is the legal property of its developers, whose names
  * can be found in the AUTHORS.md file distributed with this source
  * distribution.
  *
- * OpenBlack is free software; you can redistribute it and/or
+ * openblack is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  *
- * OpenBlack is distributed in the hope that it will be useful,
+ * openblack is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OpenBlack. If not, see <http://www.gnu.org/licenses/>.
+ * along with openblack. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
