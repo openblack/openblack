ad8855a65f63d68bfb23049f65cc79b741d99a02
Sandy Carter <bwrsandman@gmail.com>
2019-09-08 14:58:29 +0200

    Gui: Convert gui to bgfx on sdl
    Remove dependency on sdl examples

---
+++ src/Graphics/Shaders/fs_imgui_image.bin.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Generated compiled shaders using shaderc with the bin2c option
+#include <generated/imgui/shaders/fs_imgui_image.glsl.bin.h>
+#include <generated/imgui/shaders/fs_imgui_image.spv.bin.h>
+#include <generated/imgui/shaders/fs_imgui_image.dx9.bin.h>
+#include <generated/imgui/shaders/fs_imgui_image.dx11.bin.h>
+#include <generated/imgui/shaders/fs_imgui_image.mtl.bin.h>
+
