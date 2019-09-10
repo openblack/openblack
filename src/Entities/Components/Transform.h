1eeec72bdf3e441b479d1ae0413ed154ad51cc38
Matt Stevens <matt@handsomematt.co.uk>
2019-09-10 02:01:15 +0100

    Transform component: use glm::vec3

--- src/Entities/Components/Transform.h
+++ src/Entities/Components/Transform.h
@@ -1,10 +1,35 @@
+/* openblack - A reimplementation of Lionhead's Black & White.
+ *
+ * openblack is the legal property of its developers, whose names
+ * can be found in the AUTHORS.md file distributed with this source
+ * distribution.
+ *
+ * openblack is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * openblack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with openblack. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace openblack
+{
+
 struct Transform
 {
-	float x;
-	float y;
-	float z;
-	float scale;
-	float rotX;
-	float rotY;
-	float rotZ;
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scale;
 };
+
+}
