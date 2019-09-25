65c0ada8cc123d52db1b1009d182cf9580e85ed9
Sandy Carter <bwrsandman@gmail.com>
2019-09-25 21:06:11 +0200

    Vertex: clean-up
    Remove opengl calls

--- src/Graphics/VertexBuffer.cpp
+++ src/Graphics/VertexBuffer.cpp
@@ -23,8 +23,6 @@
 #include <cassert>
 #include <array>
 
-#include <Graphics/OpenGL.h>
-
 using namespace openblack::graphics;
 
 namespace {
@@ -58,11 +56,10 @@ constexpr std::array<bgfx::Attrib::Enum, 18> attributes {
 }
 
 VertexBuffer::VertexBuffer(const void* vertices, size_t vertexCount, VertexDecl decl)
-	: _vbo()
-	, _vertexCount(vertexCount)
+	: _vertexCount(vertexCount)
 	, _vertexDecl(std::move(decl))
 	, _strideBytes(0)
-	, _bgfxHandle(BGFX_INVALID_HANDLE)
+	, _handle(BGFX_INVALID_HANDLE)
 {
 	// assert(vertices != nullptr);
 	assert(vertexCount > 0);
@@ -88,35 +85,15 @@ VertexBuffer::VertexBuffer(const void* vertices, size_t vertexCount, VertexDecl 
 	assert(layout.m_stride == _strideBytes);
 
 	auto mem = bgfx::makeRef(vertices, vertexCount * layout.m_stride);
-	_bgfxHandle = bgfx::createVertexBuffer(mem, layout);
+	_handle = bgfx::createVertexBuffer(mem, layout);
 	bgfx::frame();
-
-	glGenBuffers(1, &_vbo);
-	if (glGetError() != GL_NO_ERROR)
-		return;
-
-	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-	glBufferData(GL_ARRAY_BUFFER, vertexCount * _strideBytes, vertices, GL_STATIC_DRAW);
 }
 
 VertexBuffer::~VertexBuffer()
 {
-	if (_vbo != 0)
-		glDeleteBuffers(1, &_vbo);
-	if (bgfx::isValid(_bgfxHandle))
-		bgfx::destroy(_bgfxHandle);
+	if (bgfx::isValid(_handle))
+		bgfx::destroy(_handle);
 }
-
-/*const std::unique_ptr<GLvoid*> VertexBuffer::GetData() const
-{
-	size_t dataSize = _vertexCount * _strideBytes;
-	auto data = std::make_unique<GLvoid*>(dataSize);
-
-	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-	glGetBufferSubData(_vbo, 0, dataSize, data.get());
-
-	return std::move(data);
-}*/
 
 size_t VertexBuffer::GetVertexCount() const noexcept
 {
@@ -135,28 +112,5 @@ size_t VertexBuffer::GetSizeInBytes() const noexcept
 
 void VertexBuffer::Bind()
 {
-	bgfx::setVertexBuffer(0, _bgfxHandle);
-}
-
-void VertexBuffer::bindVertexDecl()
-{
-	static const std::array<GLenum , 3> types = {
-		GL_UNSIGNED_BYTE, // Uint8
-		GL_SHORT, // Int16
-		GL_FLOAT, // Float
-	};
-	for (size_t i = 0; i < _vertexDecl.size(); ++i)
-	{
-		auto type = types[static_cast<size_t>(_vertexDecl[i]._type)];
-		if (_vertexDecl[i]._asInt)
-		{
-			glVertexAttribIPointer(i, _vertexDecl[i]._num, type, _strideBytes, _vertexDeclOffsets[i]);
-		}
-		else
-		{
-			glVertexAttribPointer(i, _vertexDecl[i]._num, type, _vertexDecl[i]._normalized, _strideBytes, _vertexDeclOffsets[i]);
-		}
-
-		glEnableVertexAttribArray(i);
-	}
+	bgfx::setVertexBuffer(0, _handle);
 }
