a72171745b372a5170d86322ea59700a07bd0e77
Sandy Carter <bwrsandman@gmail.com>
2019-09-28 11:28:12 +0200

    Shaders: simplfy compilation process remove duplicates

--- src/Graphics/Shaders/fs_ocornut_imgui.bin.h
+++ src/Graphics/Shaders/fs_ocornut_imgui.bin.h
@@ -1,8 +1,12 @@
 #pragma once
 
 // Generated compiled shaders using shaderc with the bin2c option
-#include <generated/imgui/shaders/fs_ocornut_imgui.glsl.bin.h>
-#include <generated/imgui/shaders/fs_ocornut_imgui.spv.bin.h>
-#include <generated/imgui/shaders/fs_ocornut_imgui.dx9.bin.h>
-#include <generated/imgui/shaders/fs_ocornut_imgui.dx11.bin.h>
-#include <generated/imgui/shaders/fs_ocornut_imgui.mtl.bin.h>
+#include <generated/imgui/shaders/fs_ocornut_imgui.sc.glsl.bin.h>
+#include <generated/imgui/shaders/fs_ocornut_imgui.sc.spv.bin.h>
+#if defined(_WIN32)
+#include <generated/imgui/shaders/fs_ocornut_imgui.sc.dx9.bin.h>
+#include <generated/imgui/shaders/fs_ocornut_imgui.sc.dx11.bin.h>
+#endif  //  defined(_WIN32)
+#if __APPLE__
+#include <generated/imgui/shaders/fs_ocornut_imgui.sc.mtl.bin.h>
+#endif // __APPLE__
