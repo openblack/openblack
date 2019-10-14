44660a45a5d230803ff9a013c09b4378b2ad938d
gavxn <gavxn@zanidrak.com>
2019-10-14 21:52:33 +0100

    Use enTT's context system to manage script ID to entity ID lookups

--- src/Entities/Registry.h
+++ src/Entities/Registry.h
@@ -8,27 +8,45 @@ class Camera;
 namespace openblack::graphics
 {
 class ShaderManager;
-} // namespace openblack
+} // namespace openblack::graphics
 
 namespace openblack::Entities
 {
+class RegistryContext
+{
+  public:
+	std::unordered_map<int, entt::entity> streams;
+	std::unordered_map<int, entt::entity> towns;
+};
+
 class Registry
 {
   public:
+	Registry()
+	{
+		_registry.set<RegistryContext>();
+	}
+
 	void DebugCreateEntities(float x, float y, float z);
 	void DrawModels(uint8_t viewId, graphics::ShaderManager &shaderManager);
 	void Update();
 	decltype(auto) Create() { return _registry.create(); }
 	template <typename Component, typename... Args>
 	decltype(auto) Assign(entt::entity entity, [[maybe_unused]] Args&&... args) { return _registry.assign<Component>(entity, std::forward<Args>(args)...); }
-	void Reset() { _registry.reset(); };
+	void Reset()
+	{
+		_registry.reset();
+		_registry.unset<RegistryContext>();
+		_registry.set<RegistryContext>();
+	};
 	template <typename Component>
 	size_t Size() { return _registry.size<Component>(); }
 	template <typename Component>
 	decltype(auto) Get(entt::entity entity) { return _registry.get<Component>(entity); }
+	decltype(auto) Context() { return _registry.ctx<RegistryContext>(); }
 
   private:
 	entt::registry _registry;
 };
 
-} // namespace openblack
+} // namespace openblack::Entities
