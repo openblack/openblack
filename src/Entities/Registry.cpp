5750523bc2cd3081b00351ca71e2ef28113055aa
Matt Stevens <matt@handsomematt.co.uk>
2019-10-20 22:10:06 +0100

    we won't cull faces, bw uses double sided for meshes

--- src/Entities/Registry.cpp
+++ src/Entities/Registry.cpp
@@ -26,7 +26,6 @@ void Registry::DrawModels(uint8_t viewId, graphics::ShaderManager &shaderManager
 	uint64_t state = 0u
 		| BGFX_STATE_WRITE_MASK
 		| BGFX_STATE_DEPTH_TEST_LESS
-		// | BGFX_STATE_CULL_CCW  // TODO(bwrsandman): Some meshes wind one way and some others (i.e. rocks, gate)
 		| BGFX_STATE_MSAA
 		| BGFX_STATE_BLEND_ALPHA
 	;
