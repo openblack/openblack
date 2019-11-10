7460b3c9859b992a08042ea8a256238b0016d192
Matt Stevens <matt@handsomematt.co.uk>
2019-11-10 21:57:03 +0000

    move comments in landblock

--- src/3D/LandBlock.cpp
+++ src/3D/LandBlock.cpp
@@ -103,18 +103,18 @@ void LandBlock::BuildMesh(LandIsland& island)
 	VertexDecl decl;
 	decl.reserve(7);
 	decl.emplace_back(VertexAttrib::Attribute::Position, 3, VertexAttrib::Type::Float);
-	decl.emplace_back(VertexAttrib::Attribute::TexCoord1, 3,
-	                  VertexAttrib::Type::Float); // weight
-	decl.emplace_back(VertexAttrib::Attribute::Color1, 3,
-	                  VertexAttrib::Type::Uint8); // first material id
-	decl.emplace_back(VertexAttrib::Attribute::Color2, 3,
-	                  VertexAttrib::Type::Uint8); // second material id
-	decl.emplace_back(VertexAttrib::Attribute::TexCoord2, 3, VertexAttrib::Type::Uint8,
-	                  true); // material blend coefficient
-	decl.emplace_back(VertexAttrib::Attribute::Color0, 4, VertexAttrib::Type::Uint8,
-	                  true); // light level, align to 4 bytes
-	decl.emplace_back(VertexAttrib::Attribute::Color3, 1, VertexAttrib::Type::Float,
-	                  true); // water alpha
+	// weight
+	decl.emplace_back(VertexAttrib::Attribute::TexCoord1, 3, VertexAttrib::Type::Float);
+	// first material id
+	decl.emplace_back(VertexAttrib::Attribute::Color1, 3, VertexAttrib::Type::Uint8);
+	// second material id
+	decl.emplace_back(VertexAttrib::Attribute::Color2, 3, VertexAttrib::Type::Uint8);
+	// material blend coefficient
+	decl.emplace_back(VertexAttrib::Attribute::TexCoord2, 3, VertexAttrib::Type::Uint8, true);
+	// light level, align to 4 bytes
+	decl.emplace_back(VertexAttrib::Attribute::Color0, 4, VertexAttrib::Type::Uint8, true);
+	// water alpha
+	decl.emplace_back(VertexAttrib::Attribute::Color3, 1, VertexAttrib::Type::Float, true);
 
 	auto verts = buildVertexList(island);
 
