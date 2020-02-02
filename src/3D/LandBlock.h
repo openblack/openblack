a4759002792a4b62f238df8ee4fdabdd35d4e8b3
Sandy Carter <bwrsandman@gmail.com>
2020-02-02 23:05:17 +0100

    dynamics: Mesh collision object from land blocks

--- src/3D/LandBlock.h
+++ src/3D/LandBlock.h
@@ -16,6 +16,9 @@
 
 #include <cstdint>
 
+class btBvhTriangleMeshShape;
+class btRigidBody;
+
 namespace openblack
 {
 
@@ -42,6 +45,11 @@ struct LandVertex
 
 class LandIsland;
 
+namespace dynamics
+{
+class LandBlockBulletMeshInterface;
+}
+
 class LandBlock
 {
 public:
@@ -53,10 +61,14 @@ public:
 	[[nodiscard]] const lnd::LNDCell* GetCells() const;
 	[[nodiscard]] glm::ivec2 GetBlockPosition() const;
 	[[nodiscard]] glm::vec4 GetMapPosition() const;
+	[[nodiscard]] std::unique_ptr<btRigidBody>& GetRigidBody() { return _rigidBody; };
 
 private:
 	std::unique_ptr<lnd::LNDBlock> _block;
 	std::unique_ptr<graphics::Mesh> _mesh;
+	std::unique_ptr<dynamics::LandBlockBulletMeshInterface> _dynamicsMeshInterface;
+	std::unique_ptr<btBvhTriangleMeshShape> _physicsMesh;
+	std::unique_ptr<btRigidBody> _rigidBody;
 
 	const bgfx::Memory* buildVertexList(LandIsland& island);
 
