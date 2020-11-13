5111ae1a24d0a0308a326b1c6c9728ae0a19f81c
Sandy Carter <bwrsandman@gmail.com>
2020-11-12 22:45:54 -0500

    ui: factor out Land Island Window

--- src/Gui.h
+++ src/Gui.h
@@ -89,6 +89,7 @@ private:
 
 	bool ShowMenu(Game& game);
 	void ShowProfilerWindow(Game& game);
+	void ShowLandIslandWindow(Game& game);
 	void ShowCameraPositionOverlay(const Game& game);
 
 	static const char* StaticGetClipboardText(void* ud) { return reinterpret_cast<Gui*>(ud)->GetClipboardText(); }
