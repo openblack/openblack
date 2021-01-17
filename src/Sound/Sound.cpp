0d77d3277e44431b0a24fda572795f1603c2e76c
raffclar <gavxn@zanidrak.com>
2021-01-17 06:49:23 +0000

    Add Sound class

---
+++ src/Sound/Sound.cpp
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2020 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#include "Sound.h"
---
+++ src/Sound/Sound.h
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2020 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace openblack::audio
+{
+
+using SoundId = std::string;
+
+enum class PlayType
+{
+	None,
+	Repeat,
+	Once,
+	Overlap
+};
+
+enum class ChannelLayout
+{
+	Mono,
+	Stereo
+};
+
+struct Sound
+{
+	SoundId id;
+	std::string name;
+	int priority;
+	int sampleRate;
+	int bitRate;
+	float volume;
+	int pitch;
+	int pitchDeviation;
+	std::vector<uint8_t> bytes;
+	ChannelLayout channelLayout;
+	PlayType playType;
+	bool loaded;
+	bool sectorMerged;
+	int sectorCount;
+	float lengthInSeconds;
+};
+
+using SoundMap = std::map<SoundId, std::unique_ptr<Sound>>;
+} // namespace openblack::audio
