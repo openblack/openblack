aa9fafb4624b71aaf5f7cf6c7eb08f92421d63bb
raffclar <gavxn@zanidrak.com>
2021-01-23 18:06:45 +0000

    Disable hand grab noises

--- src/Game.cpp
+++ src/Game.cpp
@@ -140,14 +140,14 @@ bool Game::ProcessEvents(const SDL_Event& event)
 		// Hand is still considered up
 		if (event.type == SDL_MOUSEBUTTONDOWN && !leftMouseButton)
 		{
-			auto& handTransform = _entityRegistry->Get<openblack::entities::components::Transform>(_handEntity);
-			auto& hand = _entityRegistry->Get<openblack::entities::components::Hand>(_handEntity);
-			auto velocity = glm::vec3(.0f);
-			auto direction = glm::vec3(0);
-			auto radius = glm::vec2(1.f);
-			auto emitterId = _soundHandler->CreateEmitters(hand.GrabLandSoundIds(), handTransform.position, velocity, direction,
-			                                               radius, 1.f, false, audio::PlayType::Once);
-			_soundHandler->PlayEmitter(emitterId);
+//			auto& handTransform = _entityRegistry->Get<openblack::entities::components::Transform>(_handEntity);
+//			auto& hand = _entityRegistry->Get<openblack::entities::components::Hand>(_handEntity);
+//			auto velocity = glm::vec3(.0f);
+//			auto direction = glm::vec3(0);
+//			auto radius = glm::vec2(1.f);
+//			auto emitterId = _soundHandler->CreateEmitters(hand.GrabLandSoundIds(), handTransform.position, velocity, direction,
+//			                                               radius, 1.f, false, audio::PlayType::Once);
+//			_soundHandler->PlayEmitter(emitterId);
 		}
 
 		leftMouseButton = !leftMouseButton;
