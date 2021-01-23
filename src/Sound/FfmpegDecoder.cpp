e65c583d97a0396d8f0566a4e64a09f9c54aa3a1
raffclar <gavxn@zanidrak.com>
2021-01-23 01:36:31 +0000

    Remove a bunch of bugs from the FFmpeg decoder

--- src/Sound/FfmpegDecoder.cpp
+++ src/Sound/FfmpegDecoder.cpp
@@ -10,6 +10,8 @@
 #include "FfmpegDecoder.h"
 #include "Common/MemoryStream.h"
 
+#include <spdlog/spdlog.h>
+#include <array>
 #include <fstream>
 #include <functional>
 
@@ -18,7 +20,21 @@
 namespace openblack::audio
 {
 
-int readSoundBytes(void* opaque, uint8_t* buffer, int bufferSize)
+std::string GetErrorCode(int errorCode)
+{
+	std::array<char, 1024> buffer;
+	std::memset(buffer.data(), 0, 1024);
+
+	if (av_strerror(errorCode, buffer.data(), buffer.size()) < 0)
+	{
+		return std::string("Unknown error code: ") + std::to_string(errorCode);
+	}
+
+	return std::string(buffer.data());
+}
+
+
+int ReadSoundBytes(void* opaque, uint8_t* buffer, int bufferSize)
 {
 	auto stream = reinterpret_cast<MemoryStream*>(opaque);
 	memset(buffer, 0, bufferSize);
@@ -45,7 +61,7 @@ void FfmpegDecoder::ToPCM16(Sound& sound)
 
 	auto memoryStream = MemoryStream(soundBytes.data(), soundBytes.size());
 	auto avioCtx = std::unique_ptr<AVIOContext, decltype(&AvDeleteAvioContext)>(
-	    avio_alloc_context(avBuffer.get(), BUFFER_SIZE, 0, &memoryStream, readSoundBytes, nullptr, nullptr),
+	    avio_alloc_context(avBuffer.get(), BUFFER_SIZE, 0, &memoryStream, ReadSoundBytes, nullptr, nullptr),
 	    AvDeleteAvioContext);
 
 	// AVIO context holds a reference to the buffer. We can now discard this one
@@ -70,15 +86,18 @@ void FfmpegDecoder::ToPCM16(Sound& sound)
 	auto codecContext = avcodec_alloc_context3(nullptr);
 	avcodec_parameters_to_context(codecContext, stream->codecpar);
 	auto decoder = avcodec_find_decoder(codecContext->codec_id);
-	avcodec_open2(codecContext, decoder, nullptr);
+	if (avcodec_open2(codecContext, decoder, nullptr) < 0)
+	{
+		throw std::runtime_error("FFMPEG error");
+	}
 
 	// This is our input
-	auto inChannels = codecContext->channel_layout == 0 ? AV_CH_LAYOUT_MONO : codecContext->channel_layout;
+	auto inChannels = codecContext->channel_layout == 0 ? av_get_default_channel_layout(codecContext->channels) : codecContext->channel_layout;
 	auto inFormat = codecContext->sample_fmt;
 	auto inSampleRate = codecContext->sample_rate;
 
 	// This is our result. We need it as signed 16-bits non-planar for our audio players
-	auto outChannels = codecContext->channel_layout == 0 ? AV_CH_LAYOUT_MONO : codecContext->channel_layout;
+	auto outChannels = inChannels;
 	auto outFormat = AV_SAMPLE_FMT_S16;
 	auto outSampleRate = codecContext->sample_rate;
 
@@ -97,26 +116,58 @@ void FfmpegDecoder::ToPCM16(Sound& sound)
 
 	while (av_read_frame(avFormatCtx.get(), packet.get()) >= 0)
 	{
-		avcodec_send_packet(codecContext, packet.get());
-		avcodec_receive_frame(codecContext, frame.get());
+		int result;
 
-		if (frame->nb_samples == 0)
-			continue;
-
-		auto outSamplesCount = swr_get_out_samples(swr.get(), frame->nb_samples);
-		auto outBufferSize = av_samples_get_buffer_size(nullptr, frame->channels, outSamplesCount, outFormat, 0);
-		auto outBuffer = std::vector<uint8_t>(outBufferSize);
-		auto rawOutBuffer = outBuffer.data();
-		memset(rawOutBuffer, 0, outBufferSize);
-		swr_convert(swr.get(), &rawOutBuffer, outSamplesCount, (uint8_t const**)frame->data, frame->nb_samples);
-		duration += frame->pkt_duration;
-
-		if (frame->data[0] == nullptr)
+		if ((result = avcodec_send_packet(codecContext, packet.get())) < 0)
 		{
-			throw std::runtime_error("No data in audio frame");
+			if (result == AVERROR_INVALIDDATA)
+			{
+				spdlog::warn("Invalid data found for " + sound.name);
+				continue;
+			}
+
+			auto i = GetErrorCode(result);
+			throw std::runtime_error("FFMPEG error: " + i);
 		}
 
-		std::copy(rawOutBuffer, rawOutBuffer + outBufferSize, std::back_inserter(loaded));
+		while((result = avcodec_receive_frame(codecContext, frame.get())) == 0 || result == -11)
+		{
+			if (result < 0 && result != AVERROR(EAGAIN) && result != AVERROR_EOF)
+			{
+				throw std::runtime_error("FFMPEG error: " + GetErrorCode(result));
+			} else if (result < 0)
+			{
+				// FFmpeg is telling us to feed it more packets
+				break;
+			}
+
+			if (frame->channels <= 0 || frame->channels > 2 || frame->sample_rate <= 0)
+			{
+				throw std::runtime_error("Invalid audio properties detected");
+			}
+
+			auto outBuffer = std::vector<uint8_t>(8192 * 2 * 8);
+			auto *out = outBuffer.data();
+
+			if ((result = swr_convert(swr.get(), &out, outBuffer.size() / (2 * frame->channels), (const uint8_t**)frame->extended_data, frame->nb_samples)) < 0)
+			{
+				throw std::runtime_error("FFMPEG error: " + GetErrorCode(result));
+			}
+
+			if (frame->data[0] == nullptr)
+			{
+				throw std::runtime_error("No data in audio frame");
+			}
+
+			std::copy(outBuffer.begin(), outBuffer.begin() + (result * 2 * frame->channels), std::back_inserter(loaded));
+		}
+
+		if (result < 0 && result != AVERROR(EAGAIN))
+		{
+			throw std::runtime_error("FFMPEG error: " + GetErrorCode(result));
+		}
+
+		duration += frame->pkt_duration;
 	}
 
 	sound.bytes = loaded;
