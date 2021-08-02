1baa37690f4e75f7292d408b37590d24c5c57ad7
raffclar <gavxn@zanidrak.com>
2021-08-02 09:57:21 +0100

    Add FileSystem::GetAllFilePaths() which searches all sub directories for files that have the matching filename extension

--- src/Common/FileSystem.h
+++ src/Common/FileSystem.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "FileStream.h"
+#include "StringUtilities.h"
 
 #include <cstddef>
 #include <list>
@@ -53,9 +54,40 @@ public:
 	void SetGamePath(const fs::path& path) { _gamePath = path; }
 	[[nodiscard]] const fs::path& GetGamePath() const { return _gamePath; }
 
+	std::vector<std::filesystem::path> GetAllFilePaths(const std::string& path, const std::string& ext, bool recursive) const
+	{
+		auto foundPath = FindPath(path);
+		if (recursive)
+			return GetAllFilePaths(std::filesystem::recursive_directory_iterator(foundPath), ext);
+		else
+			return GetAllFilePaths(std::filesystem::directory_iterator(foundPath), ext);
+	}
+
 	std::vector<uint8_t> ReadAll(const fs::path& path);
 
 private:
+	template <typename DirectoryIter>
+	std::vector<std::filesystem::path> GetAllFilePaths(DirectoryIter it, const std::string& ext) const
+	{
+		std::vector<std::filesystem::path> paths;
+
+		for (auto entry : it)
+		{
+			if (entry.is_directory())
+				continue;
+
+			auto curExt = entry.path().extension().string();
+			LowerCase(curExt);
+
+			if (curExt != ext)
+				continue;
+
+			paths.push_back(entry.path());
+		}
+
+		return paths;
+	}
+
 	fs::path _gamePath;
 };
 
