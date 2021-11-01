904fa2279cc52852000f9d6972453f6067ade988
Sandy Carter <bwrsandman@gmail.com>
2021-11-01 09:37:36 -0400

    ECS: Add big forest archetype

---
+++ src/ECS/Archetypes/BigForestArchetype.cpp
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2021 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#include "BigForestArchetype.h"
+
+#include <glm/gtx/euler_angles.hpp>
+
+#include "ECS/Components/Forest.h"
+#include "ECS/Components/Mesh.h"
+#include "ECS/Components/Transform.h"
+#include "ECS/Registry.h"
+#include "Game.h"
+
+using namespace openblack;
+using namespace openblack::ecs::archetypes;
+using namespace openblack::ecs::components;
+
+entt::entity BigForestArchetype::Create(const glm::vec3& position, BigForestInfo type, uint32_t unknown, float yAngleRadians,
+                                        float scale)
+{
+	auto& registry = Game::instance()->GetEntityRegistry();
+	const auto entity = registry.Create();
+
+	const auto& info = Game::instance()->GetInfoConstants().bigForest[static_cast<size_t>(type)];
+
+	registry.Assign<Transform>(entity, position, glm::eulerAngleY(-yAngleRadians), glm::vec3(scale));
+	registry.Assign<Forest>(entity);
+	registry.Assign<BigForest>(entity);
+	registry.Assign<Mesh>(entity, info.meshId, static_cast<int8_t>(0), static_cast<int8_t>(1));
+
+	return entity;
+}
