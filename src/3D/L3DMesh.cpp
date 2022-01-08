b6d9ee5949bc51e1d5c8ac1227f1bf9d07bfcf72
raffclar <gavxn@zanidrak.com>
2022-01-08 21:28:15 +0000

    Skip sub meshes that fail to load instead of assuming they've succeeded

--- src/3D/L3DMesh.cpp
+++ src/3D/L3DMesh.cpp
@@ -67,11 +67,15 @@ void L3DMesh::Load(const l3d::L3DFile& l3d)
 		matrices.emplace(i, matrix);
 	}
 
-	_subMeshes.resize(l3d.GetSubmeshHeaders().size());
-	for (uint32_t i = 0; i < _subMeshes.size(); ++i)
+	auto submeshCount = l3d.GetSubmeshHeaders().size();
+	for (uint32_t i = 0; i < submeshCount; ++i)
 	{
-		_subMeshes[i] = std::make_unique<L3DSubMesh>(*this);
-		_subMeshes[i]->Load(l3d, i);
+		auto subMesh = std::make_unique<L3DSubMesh>(*this);
+		if (!subMesh->Load(l3d, i))
+		{
+			continue;
+		}
+		_subMeshes.emplace_back(std::move(subMesh));
 	}
 	// TODO(bwrsandman): store vertex and index buffers at mesh level
 	bgfx::frame();
