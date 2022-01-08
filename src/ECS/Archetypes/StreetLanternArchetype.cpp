b497e0eb6b455541da81fc32580a8963a8b41b52
raffclar <gavxn@zanidrak.com>
2022-01-08 16:40:09 +0000

    Add archetypes for lanterns and bonfires

---
+++ src/ECS/Archetypes/StreetLanternArchetype.cpp
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2021 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#include "StreetLanternArchetype.h"
+
+#include <glm/gtx/euler_angles.hpp>
+
+#include "AllMeshes.h"
+#include "ECS/Components/Mesh.h"
+#include "ECS/Components/Transform.h"
+#include "ECS/Registry.h"
+#include "Game.h"
+
+using namespace openblack;
+using namespace openblack::ecs::archetypes;
+using namespace openblack::ecs::components;
+
+entt::entity StreetLanternArchetype::Create(const glm::vec3& position)
+{
+	auto& registry = Game::instance()->GetEntityRegistry();
+	const auto entity = registry.Create();
+	registry.Assign<Transform>(entity, position, glm::eulerAngleY(glm::radians(180.0f)), glm::vec3(1.0f));
+	registry.Assign<Mesh>(entity, MeshId::ObjectTownLight, static_cast<int8_t>(0), static_cast<int8_t>(1));
+	return entity;
+}
