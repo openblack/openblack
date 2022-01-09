e8e8431c0f813fdec2c089d609dab241f7c8ad0d
Sandy Carter <bwrsandman@gmail.com>
2022-01-09 14:42:23 -0500

    replace 2018-2021 with 2018-2022
    actual command:
    
        git grep -lz '2018-2021'  | xargs -0r sed -i 's/2018-2021/2018-2022/'

--- components/morph/src/MorphFile.cpp
+++ components/morph/src/MorphFile.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018-2021 openblack developers
+ * Copyright (c) 2018-2022 openblack developers
  *
  * For a complete list of all authors, please refer to contributors.md
  * Interested in contributing? Visit https://github.com/openblack/openblack
