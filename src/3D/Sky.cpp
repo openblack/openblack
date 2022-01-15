6e1e7e7ee19582861a69162e12193b6d303e0498
Sandy Carter <bwrsandman@gmail.com>
2022-01-14 22:09:03 -0500

    sky: flaten _bitmap to a 1D array

--- src/3D/Sky.cpp
+++ src/3D/Sky.cpp
@@ -19,9 +19,6 @@
 #include "Game.h"
 #include "Graphics/Texture2D.h"
 
-constexpr std::array<std::string_view, 3> alignments = {"Ntrl", "good", "evil"};
-constexpr std::array<std::string_view, 3> times = {"day", "dusk", "night"};
-
 using namespace openblack::graphics;
 
 namespace openblack
@@ -36,16 +33,16 @@ Sky::Sky()
 	_model = std::make_unique<L3DMesh>("Sky");
 	_model->LoadFromFile(filesystem.WeatherSystemPath() / "sky.l3d");
 
-	for (uint32_t i = 0; i < alignments.size(); i++)
+	for (uint32_t i = 0; i < _alignments.size(); i++)
 	{
-		for (uint32_t j = 0; j < times.size(); j++)
+		for (uint32_t j = 0; j < _times.size(); j++)
 		{
-			auto time = std::string(times[j]);
+			auto time = std::string(_times[j]);
 			if (i == 0)
 			{
 				time[0] = static_cast<char>(std::toupper(static_cast<unsigned char>(time[0])));
 			}
-			std::string filename = fmt::format("sky_{}_{}.555", alignments[i], time);
+			std::string filename = fmt::format("sky_{}_{}.555", _alignments[i], time);
 			if (i == 0)
 			{
 				filename[0] = static_cast<char>(std::toupper(static_cast<unsigned char>(filename[0])));
@@ -54,7 +51,7 @@ Sky::Sky()
 			SPDLOG_LOGGER_DEBUG(spdlog::get("game"), "Loading sky texture: {}", path.generic_string());
 
 			Bitmap16B* bitmap = Bitmap16B::LoadFromFile(path);
-			memcpy(_bitmaps[i * 3 + j].data(), bitmap->Data(), bitmap->Size());
+			memcpy(&_bitmaps[(i * 3 + j) * _textureResolution[0] * _textureResolution[1]], bitmap->Data(), bitmap->Size());
 			delete bitmap;
 		}
 	}
@@ -80,7 +77,7 @@ void Sky::SetTime(float time)
 
 void Sky::Interpolate555Texture(uint16_t* bitmap, uint16_t* bitmap1, uint16_t* bitmap2, float interpolate)
 {
-	for (int i = 0; i < 256 * 256; i++)
+	for (uint32_t i = 0; i < static_cast<uint32_t>(_textureResolution[0]) * static_cast<uint32_t>(_textureResolution[1]); i++)
 	{
 		const auto color1 = glm::u16vec3((bitmap1[i] & 0x7C00) >> 10, (bitmap1[i] & 0x3E0) >> 5, (bitmap1[i] & 0x1F));
 		const auto color2 = glm::u16vec3((bitmap2[i] & 0x7C00) >> 10, (bitmap2[i] & 0x3E0) >> 5, (bitmap2[i] & 0x1F));
@@ -91,11 +88,11 @@ void Sky::Interpolate555Texture(uint16_t* bitmap, uint16_t* bitmap1, uint16_t* b
 
 void Sky::CalculateTextures()
 {
-	std::array<uint16_t, 256 * 256> bitmap;
+	std::array<uint16_t, static_cast<size_t>(_textureResolution[0]) * static_cast<size_t>(_textureResolution[1])> bitmap;
 
-	uint16_t* day = _bitmaps[0].data();
-	uint16_t* dusk = _bitmaps[1].data();
-	uint16_t* night = _bitmaps[2].data();
+	uint16_t* day = &_bitmaps[0 * _textureResolution[0] * _textureResolution[1]];
+	uint16_t* dusk = &_bitmaps[1 * _textureResolution[0] * _textureResolution[1]];
+	uint16_t* night = &_bitmaps[2 * _textureResolution[0] * _textureResolution[1]];
 
 	// 0.00: day
 	// 0.25: dusk
@@ -112,9 +109,12 @@ void Sky::CalculateTextures()
 		Interpolate555Texture(bitmap.data(), dusk, day, (_timeOfDay - 0.75f) * 4.0f);
 
 	// set alpha=1
-	for (unsigned int i = 0; i < 256 * 256; i++) bitmap[i] = bitmap[i] | 0x8000;
+	for (uint32_t i = 0; i < static_cast<uint32_t>(_textureResolution[0]) * static_cast<uint32_t>(_textureResolution[1]); i++)
+	{
+		bitmap[i] = bitmap[i] | 0x8000;
+	}
 
-	_texture->Create(256, 256, 1, Format::RGB5A1, Wrapping::ClampEdge, bitmap.data(),
+	_texture->Create(_textureResolution[0], _textureResolution[1], 1, Format::RGB5A1, Wrapping::ClampEdge, bitmap.data(),
 	                 static_cast<uint32_t>(bitmap.size() * sizeof(bitmap[0])));
 }
 
