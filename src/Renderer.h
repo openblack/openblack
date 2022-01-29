d94306f689e7b527341d5aafeda8c9545985f484
raffclar <gavxn@zanidrak.com>
2022-01-29 17:57:18 +0000

    Update the renderer to use the Resource Managers

--- src/Renderer.h
+++ src/Renderer.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "Graphics/RenderPass.h"
+#include "Locator.h"
 
 #include <SDL.h>
 #include <bgfx/bgfx.h>
@@ -72,8 +73,6 @@ public:
 		const ecs::Registry& entities;
 		bool drawSprites;
 		bool drawTestModel;
-		const L3DMesh& testModel;
-		const L3DAnim& testAnimation;
 		bool drawDebugCross;
 		bool drawBoundingBoxes;
 		bool cullBack;
@@ -111,14 +110,14 @@ public:
 
 	void ConfigureView(graphics::RenderPass viewId, uint16_t width, uint16_t height) const;
 
-	void DrawScene(const MeshPack& meshPack, const DrawSceneDesc& desc) const;
-	void DrawMesh(const L3DMesh& mesh, const MeshPack& meshPack, const L3DMeshSubmitDesc& desc, uint8_t subMeshIndex) const;
+	void DrawScene(const resources::MeshManager& meshes, const resources::TextureManager& textures, const DrawSceneDesc& drawDesc) const;
+	void DrawMesh(const L3DMesh& mesh, const resources::TextureManager& textures, const L3DMeshSubmitDesc& desc, uint8_t subMeshIndex) const;
 	void Frame();
 
 private:
-	void DrawSubMesh(const L3DMesh& mesh, const L3DSubMesh& subMesh, const MeshPack& meshPack, const L3DMeshSubmitDesc& desc,
+	void DrawSubMesh(const L3DMesh& mesh, const L3DSubMesh& subMesh, const resources::TextureManager& textures, const L3DMeshSubmitDesc& desc,
 	                 bool preserveState) const;
-	void DrawPass(const MeshPack& meshPack, const DrawSceneDesc& desc) const;
+	void DrawPass(const resources::MeshManager& meshes, const resources::TextureManager& textures, const DrawSceneDesc& desc) const;
 
 	std::unique_ptr<graphics::ShaderManager> _shaderManager;
 	std::unique_ptr<BgfxCallback> _bgfxCallback;
