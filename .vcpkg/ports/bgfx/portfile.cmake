8b03cf9982015e245d963dd84fd41e8133da80fa
Sandy Carter <bwrsandman@gmail.com>
2022-03-21 21:26:55 -0400

    bgfx: Fix vcpkg android creating a dynamic library

--- .vcpkg/ports/bgfx/portfile.cmake
+++ .vcpkg/ports/bgfx/portfile.cmake
@@ -1,6 +1,33 @@
-# For a list of common variables see https://github.com/microsoft/vcpkg/blob/master/docs/maintainers/vcpkg_common_definitions.md
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
+#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#   VCPKG_TOOLCHAIN           = ON OFF
+#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
+#   BUILD_ARCH                = "Win32" "x64" "ARM"
+#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
+#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
+#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
+#   VCPKG_TARGET_IS_WINDOWS
+#   VCPKG_TARGET_IS_UWP
+#   VCPKG_TARGET_IS_LINUX
+#   VCPKG_TARGET_IS_OSX
+#   VCPKG_TARGET_IS_FREEBSD
+#   VCPKG_TARGET_IS_ANDROID
+#   VCPKG_TARGET_IS_MINGW
+#   VCPKG_TARGET_EXECUTABLE_SUFFIX
+#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
+#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
+#
+# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
 
 vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
     REPO "bkaradzic/bgfx.cmake"
@@ -39,15 +66,23 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tools       BGFX_BUILD_TOOLS
 )
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(BGFX_LIBRARY_TYPE "SHARED")
+else()
+    set(BGFX_LIBRARY_TYPE "STATIC")
+endif()
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBX_DIR=${SOURCE_PATH_BX}
         -DBIMG_DIR=${SOURCE_PATH_BIMG}
         -DBGFX_DIR=${SOURCE_PATH_BGFX}
-        -DBGFX_LIBRARY_TYPE="STATIC"
+        -DBGFX_LIBRARY_TYPE=${BGFX_LIBRARY_TYPE}
         -DBGFX_BUILD_EXAMPLES="OFF"
         ${FEATURE_OPTIONS}
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
 
 vcpkg_cmake_install()
