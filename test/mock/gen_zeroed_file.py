3d9535dba10b4330a09b3b50669f7d52c586ffa2
Sandy Carter <bwrsandman@gmail.com>
2022-04-21 20:44:29 -0400

    mock data: generate raw textures as 0ed files
    Add misc0a

--- test/mock/gen_zeroed_file.py
+++ test/mock/gen_zeroed_file.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Generate Zeroed out file.')
-    parser.add_argument('--output-file', required=True, help='Where to generate file.')
+    parser.add_argument('--output-files', required=True, help='Where to generate file.')
     parser.add_argument('--size', required=True, help="Size of file to generate")
     args = parser.parse_args()
-    main(args.output_file, int(args.size, 0))
+    for output_file in args.output_files.split(","):
+        main(output_file, int(args.size, 0))
