0bc5a01826fce980e59d6d29da6d32d1260b8f0e
raffclar <gavxn@zanidrak.com>
2022-04-27 00:36:49 +0100

    Add the LevelManager to load, access and remove Level resources

--- src/Resources/Resources.h
+++ src/Resources/Resources.h
@@ -17,6 +17,7 @@ namespace openblack::resources
 using MeshManager = ResourceManager<L3DLoader>;
 using TextureManager = ResourceManager<Texture2DLoader>;
 using AnimationManager = ResourceManager<L3DAnimLoader>;
+using LevelManager = ResourceManager<LevelLoader>;
 
 class ResourcesInterface
 {
@@ -24,6 +25,7 @@ public:
 	virtual resources::MeshManager& GetMeshes() = 0;
 	virtual resources::TextureManager& GetTextures() = 0;
 	virtual resources::AnimationManager& GetAnimations() = 0;
+	virtual resources::LevelManager& GetLevels() = 0;
 };
 
 class Resources final: public ResourcesInterface
@@ -32,10 +34,12 @@ public:
 	resources::MeshManager& GetMeshes() override { return _meshes; }
 	resources::TextureManager& GetTextures() override { return _textures; }
 	resources::AnimationManager& GetAnimations() override { return _animations; }
+	resources::LevelManager& GetLevels() override { return _levels; }
 
 private:
 	resources::MeshManager _meshes;
 	resources::TextureManager _textures;
 	resources::AnimationManager _animations;
+	resources::LevelManager _levels;
 };
 } // namespace openblack::resources
