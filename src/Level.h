b497eee67105499c3f00655316c3bb4971414356
raffclar <gavxn@zanidrak.com>
2022-04-27 00:34:01 +0100

    Add Level class

---
+++ src/Level.h
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2022 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+namespace openblack
+{
+class Level
+{
+public:
+	Level(std::string name, std::filesystem::path path, bool isCampaign)
+	    : _name(std::move(name))
+	    , _scriptPath(std::move(path))
+	    , _isCampaign(isCampaign)
+	{
+	}
+
+	[[nodiscard]] const std::string& GetName() const { return _name; };
+	[[nodiscard]] const std::filesystem::path& GetScriptPath() const { return _scriptPath; };
+	[[nodiscard]] bool IsCampaign() const { return _isCampaign; };
+
+private:
+	std::string _name;
+	std::filesystem::path _scriptPath;
+	bool _isCampaign;
+};
+} // namespace openblack
