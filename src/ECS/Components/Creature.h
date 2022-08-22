bf004045f357241b1b4feb22234ded79d4bd680a
raffclar <gavxn@zanidrak.com>
2022-08-22 02:56:27 +0100

    Spawn creatures in from LHScript

---
+++ src/ECS/Components/Creature.h
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Copyright (c) 2018-2022 openblack developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/openblack/openblack
+ *
+ * openblack is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#pragma once
+
+#include <string>
+
+#include "3D/CreatureBody.h"
+#include "Creature.h"
+#include "Enums.h"
+
+namespace openblack::ecs::components
+{
+
+struct Creature
+{
+	PlayerNames owner;
+	CreatureType species;
+	std::string mindName;
+};
+} // namespace openblack::ecs::components
