b93644865dae0ef12009d5586fd5d10deda20a05
FalseIlyu <83550517+FalseIlyu@users.noreply.github.com>
2023-07-07 02:02:41 +0200

    Faster Transformation matrix for entities (#624)
    * Faster rendering transformation matrix
    
    * Clang-Format and tidy

--- src/ECS/Systems/Implementations/RenderingSystem.cpp
+++ src/ECS/Systems/Implementations/RenderingSystem.cpp
@@ -112,12 +112,11 @@ void RenderingSystem::PrepareDrawUploadUniforms(bool drawBoundingBox)
 		    auto offset = uniformOffsets.insert(std::make_pair(mesh.id, 0));
 		    auto desc = _renderContext.instancedDrawDescs.find(mesh.id);
 
-		    glm::mat4 modelMatrix = glm::mat4(1.0f);
-		    modelMatrix = glm::translate(modelMatrix, transform.position);
-		    modelMatrix *= glm::mat4(transform.rotation);
+		    auto modelMatrix = glm::mat4(transform.rotation);
+		    modelMatrix = glm::translate(modelMatrix, transform.position * transform.rotation);
 		    modelMatrix = glm::scale(modelMatrix, transform.scale);
 
-		    uint32_t idx = desc->second.offset + offset.first->second;
+		    const uint32_t idx = desc->second.offset + offset.first->second;
 		    _renderContext.instanceUniforms[idx] = modelMatrix;
 		    if (drawBoundingBox)
 		    {
