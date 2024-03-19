9ba31e2f64beceebc8f1f1d13e812dd4368ecf2f
Sandy Carter <bwrsandman@gmail.com>
2024-03-19 00:27:53 -0400

    Camera Model: Return proper timing for animation

--- src/Camera/DefaultWorldCameraModel.h
+++ src/Camera/DefaultWorldCameraModel.h
@@ -105,6 +105,7 @@ private:
 	// TODO: Make part of optional of a struct?
 	float _originFocusDistanceAtInteractionStart = 0.0f;
 	glm::vec3 _focusAtClick = glm::vec3(0.0f, 0.0f, 0.0f);
+	std::chrono::duration<float> _elapsedTime = std::chrono::duration<float>::zero();
 
 	// For unit testing
 	friend TestDefaultCameraModel;
