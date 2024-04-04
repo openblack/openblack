935081dbb792a474a72130740bc17ac0d1049a35
FalseIlyu <ilyulemail@gmail.com>
2024-04-04 21:10:30 +0200

    Allow to parse file from istream
    Signed-off-by: FalseIlyu <ilyulemail@gmail.com>

--- components/lnd/include/LNDFile.h
+++ components/lnd/include/LNDFile.h
@@ -181,15 +181,15 @@ protected:
 	/// Error handling
 	void Fail(const std::string& msg);
 
-	/// Read file from the input source
-	virtual void ReadFile(std::istream& stream);
-
 	/// Write file to the input source
 	virtual void WriteFile(std::ostream& stream) const;
 
 public:
 	LNDFile();
 	virtual ~LNDFile();
+
+	/// Read file from the input source
+	virtual void ReadFile(std::istream& stream);
 
 	/// Read lnd file from the filesystem
 	void Open(const std::filesystem::path& filepath);
