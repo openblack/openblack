6baddc20a30094c11caadcc39517894b983f0dbc
Mats Vederhus <afr088@hotmail.com>
2024-05-22 13:26:38 +0200

    Update src/FileSystem/DefaultFileSystem.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/FileSystem/DefaultFileSystem.h
+++ src/FileSystem/DefaultFileSystem.h
@@ -24,7 +24,8 @@ namespace openblack::filesystem
 class DefaultFileSystem: public FileSystemInterface
 {
 public:
-	[[nodiscard]] auto FindPath(const std::filesystem::path& path) const->std::expected<std::filesystem::path, std::invalid_argument> override;
+	[[nodiscard]] auto FindPath(const std::filesystem::path& path) const
+	    -> std::expected<std::filesystem::path, std::invalid_argument> override;
 	[[nodiscard]] bool IsPathValid(const std::filesystem::path& path) override;
 	auto Open(const std::filesystem::path& path, Stream::Mode mode)
 	    -> std::expected<std::unique_ptr<Stream>, std::invalid_argument> override;
