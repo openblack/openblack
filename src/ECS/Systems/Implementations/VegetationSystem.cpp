be969cb45909a9e38c852ebb8014aacdcd69220e
raffclar <gavxn@zanidrak.com>
2026-04-30 02:30:56 +0100

    Remove unused sway variables and apply [[maybe_unused]]

--- src/ECS/Systems/Implementations/VegetationSystem.cpp
+++ src/ECS/Systems/Implementations/VegetationSystem.cpp
@@ -26,18 +26,15 @@ using namespace openblack::ecs::components;
 void VegetationSystem::Sway()
 {
 	auto& registry = Locator::entitiesRegistry::value();
-	registry.Each<const Transform, const Tree, Swayable>([](const Transform& transform, const Tree& tree, Swayable& swayable) {
-		// Calculate sway parameters
-		float swayStrength = swayable.swayStrength;
-		float swayOffset = swayable.swayOffset * 100.0f;
-
-		if (!swayable.swayedByHand)
-		{
-			auto elapsedTime = Locator::time::value().GetElapsedTime();
-			auto gameSpeed = Game::Instance()->GetGameSpeed();
-			auto naturalSwayTime = std::sin((elapsedTime.count() / 1000.0f * gameSpeed) + (swayable.swayOffset * 100.0f));
-			swayable.swayTime = naturalSwayTime;
-			swayable.swayStrength = std::min(swayable.swayStrength, swayable.swayStrengthBase);
-		}
-	});
+	registry.Each<const Transform, const Tree, Swayable>(
+	    []([[maybe_unused]] const Transform& transform, [[maybe_unused]] const Tree& tree, Swayable& swayable) {
+		    if (!swayable.swayedByHand)
+		    {
+			    auto elapsedTime = Locator::time::value().GetElapsedTime();
+			    auto gameSpeed = Game::Instance()->GetGameSpeed();
+			    auto naturalSwayTime = std::sin((elapsedTime.count() / 1000.0f * gameSpeed) + (swayable.swayOffset * 100.0f));
+			    swayable.swayTime = naturalSwayTime;
+			    swayable.swayStrength = std::min(swayable.swayStrength, swayable.swayStrengthBase);
+		    }
+	    });
 }
